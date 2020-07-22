12df1ce5a7d55952f8f942318d28526f13e3e67f
JunJung <keyos27@gmail.com>
2020-07-21 19:48:31 -0700

    tailwind.config

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#FF6363',
+        secondary:{
+          100:'#e2e2d5',
+          200:'#888883',
+        }
+      },
+      fontFamily: {
+        body:['Dancing Script']
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
